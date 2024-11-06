4fb1e5a7f98d900839d2697edb7d75b860c0aec4
Moayad <dmoayad@hotmail.com>
2024-11-06 22:33:25 +0300

    wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config = {
+  font = wezterm.font 'FiraCode Nerd Font',
+  font_size = 11,
+  color_scheme = 'tokyonight',
+  cursor_blink_rate = 500,
+  automatically_reload_config = true,
+  hide_tab_bar_if_only_one_tab = true,
+  -- enable_tab_bar = false,
+  -- window_decorations = "RESIZE",
+}
+
+return config
