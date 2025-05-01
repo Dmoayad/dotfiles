faef322f262d5a954711140b8f336b38ae0bb2e7
Moayad <dmoayad@hotmail.com>
2025-05-01 22:16:25 +0300

    updating wezterm and ghostty

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,16 +3,27 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-  font = wezterm.font 'FiraCode Nerd Font',
-  font_size = 11,
-  color_scheme = 'Catppuccin Mocha',
-  cursor_blink_rate = 500,
-  automatically_reload_config = true,
-  hide_tab_bar_if_only_one_tab = true,
-  initial_cols = 120,
-  initial_rows = 40,
-  -- enable_tab_bar = false,
-  -- window_decorations = "RESIZE",
+	font = wezterm.font_with_fallback({
+		{
+			family = "FiraCode Nerd Font",
+		},
+		{
+			family = "FiraCode Nerd Font",
+			assume_emoji_presentation = true,
+		},
+	}),
+
+	-- font = wezterm.font("FiraCode Nerd Font"),
+	-- font = wezterm.font(assume_emoji_presentation = true),
+	font_size = 13,
+	color_scheme = "Catppuccin Mocha",
+	cursor_blink_rate = 500,
+	automatically_reload_config = true,
+	hide_tab_bar_if_only_one_tab = true,
+	initial_cols = 120,
+	initial_rows = 40,
+	-- enable_tab_bar = false,
+	-- window_decorations = "RESIZE",
 }
 
 return config
